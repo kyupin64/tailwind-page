277943b97608d555272fa96079700c8dc105764e
kyupin64 <lewilks.321@gmail.com>
2024-02-11 17:30:03 -0700

    Add custom styles in config, minor style changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,25 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
-  },
+    extend: {
+      colors: {
+        "purple": {
+          lavender: "#efa0f7",
+          lilac: "#b660cd",
+          plum:  "#8d4585",
+          eggplant: "#311432",
+        },
+        "red": {
+          ferrari: "#ff2800",
+          cherry: "#911402",
+          mahogany: "#360904",
+        },
+      },
+      backdropBlur: {
+        xs: "2px",
+      },
+    },
   plugins: [],
+  }
 }
 
